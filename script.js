9aad6da898e09d2f9dee2034e253725ffd6e6921
21tushar <prasant324@gmail.com>
2023-12-24 12:39:55 +0530

    Clear All Items

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("item-form")
 const itemInput = document.getElementById("item-input")
 const itemList = document.getElementById("item-list")
+const clearBtn = document.getElementById('clear')
 
 const addItem = (e) => {
     e.preventDefault();
@@ -44,6 +45,13 @@ const removeItem = (e) => {
     };
 }
 
+const clearItems = () => {
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild)
+    }
+}
+
 //Event Listeners 
 itemForm.addEventListener('submit', addItem)
 itemList.addEventListener('click', removeItem)
+clearBtn.addEventListener('click', clearItems)
